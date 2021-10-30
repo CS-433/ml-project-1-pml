b254344246b994c28d138a251bfeb187d0630d65
Theo Patron <patron.theo@gmail.com>
2021-10-30 22:37:11 +0200

    propre

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -2,8 +2,6 @@ import numpy as np
 from proj1_helpers import *
 from helpers_data import *
 
-
-
 ##################### LOSSES #####################
 def compute_loss(y, tx, w):
     """Calculate the MSE loss."""
@@ -182,8 +180,8 @@ def reg_logistic_regression(y, tx, initial_w, max_iter, lambda_, gamma):
         w = w - gamma * gradient
         
         # log info
-        # if iter % 100 == 0:
-        #     print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
+        if iter % 100 == 0:
+            print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
 
         # converge criterion
         if loss < 0:
